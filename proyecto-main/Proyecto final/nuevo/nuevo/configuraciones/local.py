cc4d26b4c03eff85e2e4460088819643378af18a
juamqi <juampikass@gmail.com>
2025-08-15 20:46:27 -0300

    Configure static files and update requirements
    Added STATIC_ROOT to Django settings for static file collection. Updated allowed hosts to include juamqi.pythonanywhere.com for deployment. requirements.txt was updated with pinned versions and additional dependencies. Added staticfiles directory with admin and asset static files for deployment.

--- proyecto-main/Proyecto final/nuevo/nuevo/configuraciones/local.py
+++ proyecto-main/Proyecto final/nuevo/nuevo/configuraciones/local.py
@@ -2,7 +2,7 @@ from .settings import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'juamqi.pythonanywhere.com']
 
 DATABASES = {
     'default': {
